936e9386e459ccd05f3000bb02cfa5f3888654b8
IamMaheshGurung <gurungmahesh167@gmail.com>
2025-05-15 08:48:13 +0900

    adding loop and break while getting right guess

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -11,22 +11,32 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    let guess : u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("Your guess is: {}", guess);
+        let guess : u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You guessed it!"),
+        println!("Your guess is: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You guessed it!");
+                break;
+        
+            }
+        }
     }
 }
+
+
+    
