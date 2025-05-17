2b177c06f693d9aa8f87f25834e407701fdb3c98
IamMaheshGurung <gurungmahesh167@gmail.com>
2025-05-17 16:08:28 +0900

    variables mutability and immutability

---
+++ projects/variables/target/debug/deps/variables-0c72410d56fe118d.d
@@ -0,0 +1,5 @@
+/Users/maheshgurung67/rust/RustUp/projects/variables/target/debug/deps/libvariables-0c72410d56fe118d.rmeta: src/main.rs
+
+/Users/maheshgurung67/rust/RustUp/projects/variables/target/debug/deps/variables-0c72410d56fe118d.d: src/main.rs
+
+src/main.rs:
---
+++ projects/variables/target/debug/deps/variables-2906eca77d13259a.d
@@ -0,0 +1,5 @@
+/Users/maheshgurung67/rust/RustUp/projects/variables/target/debug/deps/libvariables-2906eca77d13259a.rmeta: src/main.rs
+
+/Users/maheshgurung67/rust/RustUp/projects/variables/target/debug/deps/variables-2906eca77d13259a.d: src/main.rs
+
+src/main.rs:
