26bb231a7ddb6f74fa6d541ed99354540c456d1e
IamMaheshGurung <gurungmahesh167@gmail.com>
2025-05-18 16:56:23 +0900

    -

--- projects/temp_converter/src/main.rs
+++ projects/temp_converter/src/main.rs
@@ -3,66 +3,104 @@ use std::io;
 
 
 fn main() {
-    println!("HI WELCOME TO TEMPRETURE CONVERTER");
-    println!("Select 1 to convert Celsius to Fahrenheit\nSelect 2 to convert Fahrenheit to Celsius\n Select other to cancel");
 
-    let mut uinput = String::new();
+    println!("___________________TEMPRETURE CONVERTER_______________");
+    println!("********************WELCOME***************************");
 
-    io::stdin()
-        .read_line(&mut uinput)
-        .expect("Failed to read line");
 
-    let uinput : u32 = match uinput.trim().parse() {
-        Ok(num) => num, 
-        Err(_) => {
-            println!("Please enter a valid number");
-            return;
+    loop {
+        println!("Select 1 to convert Celsious to fahrenheit");
+        println!("Select 2 to convert Fahrenheit to celsious");
+        println!("Select other to cancel");
+
+
+        let mut uinput = String::new();
+
+
+        io::stdin()
+            .read_line(&mut uinput)
+            .expect("Failed to read line");
+
+        let uinput: i32 = match uinput.trim().parse() {
+            Ok(num) => num, 
+            Err(_) => {
+                println!("Please enter a valid number.");
+                continue;
+            }
+
+        };
+
+        if uinput == 1 {
+            println!("ENter the number in Celsious please:  ");
+
+            let mut celsious = String::new();
+
+
+            io::stdin()
+                .read_line(&mut celsious)
+                .expect("failed to read the celsious");
+
+
+
+            let celsious: i32 = match celsious.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Please enter a valid number.");
+                    continue;
+
+                }
+            };
+
+            let fahrenheit = (celsious * 9 / 5) + 32;
+            println!("The temperature in fahrenheit is: {}", fahrenheit);
+
+
+
+        } else if uinput == 2 {
+            println!("Enter the tempreture in fahrenheit:  ");
+
+                let mut fahrenheit = String::new();
+
+                io::stdin()
+                .read_line(&mut fahrenheit)
+                .expect("failed to read the fahrenheit");
+
+
+                let fahrenheit: i32 = match fahrenheit.trim().parse() {
+                    Ok(num)=> num, 
+                    Err(_) => {
+                        println!("Please enter a valid number.");
+                        continue;
+                    }
+
+
+                };
+
+                let celsious = (fahrenheit - 32) * 5 / 9;
+                println!("The temperature in celsious is: {}", celsious);
+
+        } else {
+            println!("You have cancelled the program");
+            println!("Do you still want to continue the progream");
+            println!("If YES 'y' or 'Y' else 'n' or 'N' to exit the program");
+
+            let mut continue_program = String::new();
+            io::stdin()
+                .read_line(&mut continue_program)
+                .expect("failed to read the input");
+            let continue_program = continue_program.trim();
+            if continue_program == "y" || continue_program == "Y" {
+                println!("You have selected to continue the program");
+                continue;
+            } else if continue_program == "n" || continue_program == "N" {
+                println!("You have selected to exit the program");
+                break;
+            } else {
+                println!("Invalid input, exiting the program.");
+                break;
+            }
+            
         }
-    };
-
-
-    if uinput == 1 {
-        println!("Enter the temperature in Celsius");
-        let mut celsius = String::new();
-        io::stdin()
-            .read_line(&mut celsius)
-            .expect("Failed to read the celsius temperature");
-
-        let celsius: i32 = match celsius.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please Enter a valid number as temperature");
-                return;
-            }
-
-        };
-        let fahrenheit = (celsius * 9/5) + 32;
-        println!("The temperature in Fahrenheit is {}", fahrenheit);
-
-    } else if uinput == 2 {
-        println!("ENter the temperature in Fahrenheit");
-        let mut fahrenheit = String::new();
-
-        io::stdin()
-            .read_line(&mut fahrenheit)
-            .expect("Failed to read the fahrenheit temperature");
-
-        let fahrenheit: i32 = match fahrenheit.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please Enter a valid number as temperature");
-                return;
-            }
-        };
-        let celsius = (fahrenheit - 32) * 5/9;
-        println!("The temperature in Celsius is {}", celsius);
-    } else {
-        println!("You have cancelled the program");
     }
-    println!("Thank you for using the temperature converter");
-    println!("Have a nice day");
+    println!("Thank you for using the program");
 }
-
-
-
-
